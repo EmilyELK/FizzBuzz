0138904b084e8ef16a6f43b531d7740099706c69
Emily Krauter <mooshrimp@gmail.com>
2016-10-26 10:11:58 -0400

    Added main

---
+++ main.js
@@ -0,0 +1,11 @@
+for(var num = 1; num <= 100; num ++) {
+	if (num % 15 === 0) {
+		console.log("fizz buzz");
+	} else if (num % 5 === 0) {
+		console.log("buzz");
+	} else if (num % 3 === 0) {
+		console.log("fizz");
+	} else {
+		console.log(num);
+	}
+}
